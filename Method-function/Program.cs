47e924a853292683637f7af32a73772a857c8ac1
Seymur03082001 <116655341+Seymur03082001@users.noreply.github.com>
2022-11-17 06:52:01 +0400

    first commit

---
+++ Method-function/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+namespace Method_function
+{
+    internal class Program
+    {
+        /*static void Main(string[] args)
+        {
+            PrintConsole("Sabir");
+            PrintConsole("Sabir", "Guliyev");
+            PrintConsole("Sabir", "Guliyev", "Mehti");
+        }
+        public static void PrintConsole(string name)
+        {
+            Console.WriteLine(name);
+        }
+        public static void PrintConsole(string name,string surname)
+        {
+            Console.WriteLine(surname + " " + name);
+        }
+        public static void PrintConsole(string name,string surname,string father)
+        {
+            Console.WriteLine(name[0] + "." + surname +" "+father);
+        }*/
+
+        
+        
+        
+        
+        /*static void Main(string[] args)
+        {
+            Console.WriteLine(UpToPower(2,3));
+        }
+        public static int UpToPower(int number, int power)
+        {
+            int result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= number;
+            }
+            return result;*/
+        }
+
+    }
+}
